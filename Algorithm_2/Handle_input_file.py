9373bfa1aef2bfe3b56d8e173b348db7dab6e0dc
scpei <shichao.pei@kaust.edu.sa>
2017-11-24 11:12:58 +0300

    New One

--- Algorithm_2/Handle_input_file.py
+++ Algorithm_2/Handle_input_file.py
@@ -1,10 +1,18 @@
 file = open("256_2048.ini")
 new_line = []
+source = '257'
+sink = '258'
 for line in file:
     new = line.split('\n')[0].split(' ')
-    content = new[1] + " " + new[2] + " "+ new[4]+ " "+ new[5]+ "\n"
+    if new[1] == source:
+        new[1] = '0'
+
+    if new[2] == sink:
+        new[2] = '257'
+    content = new[1] + " " + new[2] + " " + new[4] + " " + new[5] + "\n"
     new_line.append(content)
 
 with open("Formatted_Data.txt", "a") as f:
     for item in new_line:
         f.write(item)
+    f.close()
